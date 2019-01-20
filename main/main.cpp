d41b0e2aeb703414961c89d092806522cf9b40c3
Roman Chomik <romanchom@gmail.com>
2019-01-20 10:53:29 +0100

    More stubs.

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,17 @@
-#include <mbed.h>
-#include <DigitalOut.h>
-
 #include "I2CBus.hpp"
 
 #include <icarus/I2CRegisterBank.hpp>
 #include <icarus/MPU9255.hpp>
+#include <icarus/Aircraft.hpp>
+#include <icarus/PIDController.hpp>
+#include <icarus/HoverNavigator.hpp>
+#include <icarus/KalmanFilter.hpp>
+#include <icarus/ElectricMotor.hpp>
+
+#include <mbed.h>
+#include <DigitalOut.h>
+#include <Thread.h>
+
 
 DigitalOut led1(LED1);
 
@@ -17,11 +24,36 @@ int main()
     auto mpu9255 = icarus::MPU9255(&mpu9255Device);
     mpu9255.initialize();
 
+    icarus::ElectricMotor motors[4];
+
+    auto pid = icarus::PIDController(motors, 4);
+
+    auto filter = icarus::KalmanFilter();
+
+    auto quadro = icarus::Aircraft(&pid, &filter);
+
+    auto navigator = icarus::HoverNavigator();
+
+    quadro.navigator(&navigator);
+
+    std::vector<icarus::SensorReading> readings;
+
+    auto timeToSleep = rtos::Kernel::get_ms_count();
+    constexpr auto frequency = 100;
+
     while (true) {
         mpu9255.read();
+        readings.push_back(mpu9255.acceleration());
+        readings.push_back(mpu9255.angularVelocity());
+        filter.integrateReadings(readings.data(), readings.size(), 1.0 / frequency);
+        readings.clear();
+
+        quadro.fly();
 
         printf("Hello\n\r");
         led1 = !led1;
-        wait(1.0);
+        
+        timeToSleep += 1000 / frequency;
+        ThisThread::sleep_until(timeToSleep);
     }
 }
