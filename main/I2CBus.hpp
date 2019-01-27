f1319e2cf62891f53d341d99fc745a2923a05153
Roman Chomik <romanchom@gmail.com>
2019-01-28 00:11:40 +0100

    Added barometer. Disabled usued parts of main. Fixed bug in I2CBus with 0 byte reads.

--- main/I2CBus.hpp
+++ main/I2CBus.hpp
@@ -12,12 +12,13 @@ struct I2CBus
         mDriver.frequency(400000);
     }
     
-    void transfer(uint8_t deviceAddress, std::byte const * writeBuffer, size_t writeSize, std::byte * readBuffer, size_t readSize)
+    void transfer(uint8_t const deviceAddress, std::byte const * writeBuffer, size_t const writeSize, std::byte * readBuffer, size_t const readSize)
     {
         MbedWait::microseconds(1000);
         if (mDriver.write(deviceAddress << 1, reinterpret_cast<char const *>(writeBuffer), writeSize) != 0) {
             throw std::runtime_error("I2C write failed.");
-        } 
+        }
+        if (readSize == 0) return;
         MbedWait::microseconds(1000);
         if (mDriver.read(deviceAddress << 1, reinterpret_cast<char *>(readBuffer), readSize) != 0) {
             throw std::runtime_error("I2C read failed.");
--- main/main.cpp
+++ main/main.cpp
@@ -4,6 +4,9 @@
 #include <icarus/I2CRegisterBank.hpp>
 #include <icarus/MPU9255.hpp>
 #include <icarus/AK8963.hpp>
+#include <icarus/BMP180.hpp>
+
+
 #include <icarus/Aircraft.hpp>
 #include <icarus/PIDController.hpp>
 #include <icarus/HoverNavigator.hpp>
@@ -21,62 +24,72 @@ extern "C" int main();
 
 int main()
 {
+
     led2 = 1;
+    printf("\n\rHello");
     I2CBus i2c(PB_9, PB_8);
     auto mpu9255Device = icarus::I2CRegisterBank(&i2c, 104);
     auto mpu9255 = icarus::MPU9255(&mpu9255Device);
+
     auto ak8963Device = icarus::I2CRegisterBank(&i2c, 12);
     auto ak8963 = icarus::AK8963<MbedWait, decltype(ak8963Device)>(&ak8963Device);
-    wait_ms(1000);
+
+    auto bmp180Device = icarus::I2CRegisterBank(&i2c, 119);
+    auto bmp180 = icarus::BMP180(&bmp180Device);
+
     while (true) {
         try {
+            printf("\n\rTrying to init");
             mpu9255.initialize();
             mpu9255.i2cBypass(true);
+            ak8963.initialize();
+            bmp180.initialize();
+            bmp180.startTemperatureRead();
+            wait_ms(10);
+            bmp180.readTemperature();
+            printf("%f C\n\r", bmp180.temperature());
             break;
-            // ak8963.initialize();
         } catch (std::runtime_error const & error){
             printf("\n\r%s", error.what());
         }
     }
-    
-    icarus::ElectricMotor motors[4];
 
-    auto pid = icarus::PIDController(motors, 4);
+    // icarus::ElectricMotor motors[4];
 
-    auto filter = icarus::KalmanFilter();
+    // auto pid = icarus::PIDController(motors, 4);
 
-    auto quadro = icarus::Aircraft(&pid, &filter);
+    // auto filter = icarus::KalmanFilter();
 
-    auto navigator = icarus::HoverNavigator();
+    // auto quadro = icarus::Aircraft(&pid, &filter);
 
-    quadro.navigator(&navigator);
+    // auto navigator = icarus::HoverNavigator();
 
-    std::vector<icarus::SensorReading> readings;
+    // quadro.navigator(&navigator);
+
+    // std::vector<icarus::SensorReading> readings;
 
     auto timeToSleep = rtos::Kernel::get_ms_count();
-    constexpr auto frequency = 100;
+    constexpr auto frequency = 10;
 
     while (true) {
         try {
             mpu9255.read();
-            // ak8963.read();
+            ak8963.read();
+            bmp180.readPressure();
+            bmp180.startPressureRead();
+            printf("\n\r%f Pa", bmp180.pressure());
+
         } catch (std::runtime_error const & error) {
             printf("\n\r%s", error.what());
         }
-        
-        filter.integrateReadings(readings.data(), readings.size(), 1.0 / frequency);
-        readings.clear();
+        // filter.integrateReadings(readings.data(), readings.size(), 1.0 / frequency);
+        // readings.clear();
 
-        quadro.fly();
-
-        Eigen::Matrix<float, 3, 1> mag = ak8963.mScale * 1000000;
-        printf("$");
-
-        // printf("%fuT %fuT %fuT\n\r", mag[0], mag[1], mag[2]);
+        // quadro.fly();
 
         led1 = !led1;
         led2 = !led2;
-        
+
         timeToSleep += 1000 / frequency;
         ThisThread::sleep_until(timeToSleep);
     }
